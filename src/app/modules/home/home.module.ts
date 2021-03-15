947c7a4e00b67bd8fb8af3bb77bd93f67b9d6974
Robin Genz <mail@robingenz.dev>
2021-03-15 21:53:03 +0100

    refactor: home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { HomePageRoutingModule } from './home-routing.module';
-import { HomePage } from './pages';
+import { HomePage } from './home.page';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
