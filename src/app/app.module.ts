e2fe4b5afeda8ea7430cfcbe5679e2e2ad31464d
Robin Genz <mail@robingenz.dev>
2021-03-20 22:17:00 +0100

    style: format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,14 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+  ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     {
