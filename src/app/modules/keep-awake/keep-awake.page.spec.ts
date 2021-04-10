f3f8b4252baa1b39881fa12536d2e36b5dca2d85
Robin Genz <mail@robingenz.dev>
2021-04-10 11:20:56 +0200

    test: fix

--- src/app/modules/keep-awake/keep-awake.page.spec.ts
+++ src/app/modules/keep-awake/keep-awake.page.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { SharedTestingModule } from '@tests/modules';
 import { KeepAwakePage } from './keep-awake.page';
 
 describe('KeepAwakePage', () => {
   let component: KeepAwakePage;
   let fixture: ComponentFixture<KeepAwakePage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ KeepAwakePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [KeepAwakePage],
+        imports: [SharedTestingModule],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(KeepAwakePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(KeepAwakePage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }),
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
