6985bdb21aadbaf296c32ed86671f9a519483456
Robin Genz <mail@robingenz.dev>
2021-05-06 18:22:06 +0200

    refactor: use SharedModule

--- src/app/modules/android-dark-mode-support/android-dark-mode-support.module.ts
+++ src/app/modules/android-dark-mode-support/android-dark-mode-support.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { SharedModule } from '@app/shared';
 import { AndroidDarkModeSupportPageRoutingModule } from './android-dark-mode-support-routing.module';
-
 import { AndroidDarkModeSupportPage } from './android-dark-mode-support.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    AndroidDarkModeSupportPageRoutingModule,
-  ],
+  imports: [SharedModule, AndroidDarkModeSupportPageRoutingModule],
   declarations: [AndroidDarkModeSupportPage],
 })
 export class AndroidDarkModeSupportPageModule {}
