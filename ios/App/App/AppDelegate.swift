367fcc5ff2ee8c3a32df96a3c11b441dacfae2ac
Robin Genz <mail@robingenz.dev>
2021-05-17 20:52:28 +0200

    wip

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,11 +1,15 @@
 import UIKit
 import Capacitor
+import RobingenzCapacitorScreenOrientation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+    return ScreenOrientation.getSupportedInterfaceOrientations()
+  }
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
