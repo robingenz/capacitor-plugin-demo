83a24af66629daa30d3dd42a56f5d425d8f930f0
Robin Genz <mail@robingenz.dev>
2021-08-26 21:44:50 +0200

    wip

--- src/app/modules/screen-orientation/screen-orientation.page.ts
+++ src/app/modules/screen-orientation/screen-orientation.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PluginListenerHandle } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
 import { ScreenOrientation } from '@robingenz/capacitor-screen-orientation';
 
@@ -7,11 +8,26 @@ import { ScreenOrientation } from '@robingenz/capacitor-screen-orientation';
   templateUrl: './screen-orientation.page.html',
   styleUrls: ['./screen-orientation.page.scss'],
 })
-export class ScreenOrientationPage {
+export class ScreenOrientationPage implements OnInit, OnDestroy {
+  public currentOrientation = '';
   private readonly GH_URL =
     'https://github.com/robingenz/capacitor-screen-orientation';
+  private orientationChangeListener: PluginListenerHandle | undefined;
 
   constructor(private readonly platform: Platform) {}
+
+  public ngOnInit() {
+    this.orientationChangeListener = ScreenOrientation.addListener(
+      'screenOrientationChange',
+      async ({ type }) => {
+        this.currentOrientation = type;
+      },
+    );
+  }
+
+  public ngOnDestroy() {
+    this.orientationChangeListener?.remove();
+  }
 
   public openOnGithub(): void {
     window.open(this.GH_URL, '_blank');
