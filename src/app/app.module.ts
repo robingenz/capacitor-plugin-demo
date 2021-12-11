903893aeefd525a70e86cfe47d5808bbacb05d3c
Robin Genz <mail@robingenz.dev>
2021-12-11 21:18:15 +0100

    fmt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,22 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        IonicModule.forRoot(),
-        AppRoutingModule,
-        ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: environment.production,
-        }),
-    ],
-    providers: [
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        {
-            provide: ErrorHandler,
-            useClass: GlobalErrorHandlerService,
-        },
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandlerService,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
